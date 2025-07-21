2486785ab339a0f00ca4336e7ebb62ca78c21265
davide-delgiudice <dav.del.giudice@gmail.com>
2025-07-21 17:05:44 +0200

    [UPD] question 3 risolta

--- script.js
+++ script.js
@@ -15,17 +15,17 @@
 
 // Code Question 2
 
-const hamburger = { 
-    name: "Cheese Burger", 
-    weight: 250,
-    ingredients: ["Cheese", "Meat", "Bread", "Tomato"]
-};
+// const hamburger = { 
+//     name: "Cheese Burger", 
+//     weight: 250,
+//     ingredients: ["Cheese", "Meat", "Bread", "Tomato"]
+// };
 
-const secondBurger = {...hamburger};
-secondBurger.ingredients[0] = "Salad";
+// const secondBurger = {...hamburger};
+// secondBurger.ingredients[0] = "Salad";
 
-console.log(hamburger.ingredients[0]); // Salad
-console.log(secondBurger.ingredients[0]); // Salad
+// console.log(hamburger.ingredients[0]); // Salad
+// console.log(secondBurger.ingredients[0]); // Salad
 
 // P.S.: Ricordati che gli Array, come gli oggetti, sono dei Reference Type (Tipi di Riferimento)!
 
@@ -34,3 +34,23 @@ console.log(secondBurger.ingredients[0]); // Salad
 
 
 
+// Code Question 3
+
+const hamburger = { 
+    name: "Cheese Burger", 
+    weight: 250,
+    maker: {
+        name: "Anonymous Chef",
+        restaurant: {
+            name: "Hyur's Burgers",
+            address: "Main Street, 123",
+            isOpen: true,
+        },
+        age: 29
+    }
+};
+
+const secondBurger = structuredClone(hamburger);
+const thirdBurger = structuredClone(hamburger);
+
+// Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice? Nove, tre array di oggetti con tre oggetti ciascuno;
